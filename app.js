e889ad0281dc8bb4a368a9eb3b3a649550908432
shiekhjawed <shiekhamberjawed@gmail.com>
2026-06-21 13:20:13 +0500

    clean project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 1. string
+let name = "Shiekh Amber jawed";
+// 2. number
+let sellerPrice = 1000;
+// 3. boolean (different example)
+let isRaining = true;
+// 4. object
+let user = {
+    name: "Shiekh Amber jawed",
+    city: "Karachi",
+    education: "Bachelor of Engineering"
+};
+// 5. array (colors example)
+let colors = ["Red", "Green", "Blue", "Yellow"];
+// 6. any
+let student = "Ahmed";
+let age = 22;
+let marks = 500;
+let degreeHolding = "BS Computer Science";
+// 7. null
+let value = null;
---
+++ app.ts
@@ -0,0 +1,27 @@
+// 1. string
+let name: string = "Shiekh Amber jawed";
+// 2. number
+let sellerPrice: number = 1000;
+// 3. boolean (different example)
+let isRaining: boolean = true;
+
+// 4. object
+let user: object = {
+  name: "Shiekh Amber jawed",
+  city: "Karachi",
+  education: "Bachelor of Engineering"
+};
+
+// 5. array (colors example)
+let colors: string[] = ["Red", "Green", "Blue", "Yellow"];
+
+// 6. any
+let student: any = "Ahmed";
+let age: any = 22;
+let marks: any = 500;
+let degreeHolding: any = "BS Computer Science";
+
+
+
+// 7. null
+let value: null = null;
